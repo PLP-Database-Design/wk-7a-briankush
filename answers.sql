618a5116dbee18551b69c5d6c729da6bf18d414f
Brian Kuria Mwangi <154018134+briankush@users.noreply.github.com>
2025-05-14 15:35:24 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,29 +1,77 @@
--- Question 1
--- we will create a separate table for order products and orders and link them using a foreign key as shown below
+ -- Question 1
+
+CREATE TABLE ProductDetail (
+
+    OrderID INT,
+
+    CustomerName VARCHAR(100),
+
+    Products VARCHAR(100)
+
+);
+
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+
+VALUES
+
+(101, 'John Doe', 'Laptop'),
+
+(101, 'John Doe', 'Mouse'),
+
+(102, 'Jane Smith', 'Tablet'),
+
+(102, 'Jane Smith', 'Keyboard'),
+
+(102, 'Jane Smith', 'Mouse'),
+
+(103, 'Emily Clark', 'Phone');
+
+-- Question 2
+
 CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(50)
+
+    OrderID INT PRIMARY KEY,
+
+    CustomerName VARCHAR(100)
+
 );
 
+INSERT INTO Orders (OrderID, CustomerName)
 
-CREATE TABLE OrderProducts (
-    OrderID INT,
-    Product VARCHAR(255),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+VALUES
+
+(101, 'John Doe'),
+
+(102, 'Jane Smith'),
+
+(103, 'Emily Clark');
+
+CREATE TABLE Product (
+
+    OrderID INT,
+
+    Product VARCHAR(100),
+
+    Quantity INT,
+
+    PRIMARY KEY (OrderID, Product),
+
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+
 );
 
+INSERT INTO Product (OrderID, Product, Quantity)
 
+VALUES
 
--- QUESTION 2
--- Here i will create a  table for Customer details and relating it via OrderID and Product Id
-CREATE TABLE Customers (
-    CustomerID INT AUTO_INCREMENT PRIMARY KEY,
-    CustomerName VARCHAR(100) NOT NULL
-);
+(101, 'Laptop', 2),
 
+(101, 'Mouse', 1),
 
-CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
-    CustomerID INT NOT NULL,
-    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
-);
+(102, 'Tablet', 3),
+
+(102, 'Keyboard', 1),
+
+(102, 'Mouse', 2),
+
+(103, 'Phone', 1);
